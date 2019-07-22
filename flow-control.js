4121ddee13edddfbdffc78b37182718e1b71ec46
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-07-22 23:31:14 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -3,7 +3,7 @@ function basicTeenager (age) {
 }
 function teenager(age) {
   if (age >=13 && age <=19)  {return "You are a teenager!" }
-  else {return "You are not a teenage"}
+  else {return "You are not a teenager"}
 }
 
 function ageChecker(age) {
@@ -13,7 +13,7 @@ function ageChecker(age) {
 }
 function ternaryTeenager(age) {
   return (age >=13 && age<=19) ?
-  "You are a teenage" : "You are not a teenager"
+  "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
